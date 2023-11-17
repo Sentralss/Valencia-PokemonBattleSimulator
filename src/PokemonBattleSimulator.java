dccf9f02e99af2f097b610dbbbf48395fbb345bb
BT_3W2_25 <bryantv2789@nycstudents.net>
2023-11-17 11:20:38 -0500

    initial commit

--- src/PokemonBattleSimulator.java
+++ src/PokemonBattleSimulator.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class PokemonBattleSimulator {
+public class PokemonBattleSimulator {
 }
--- src/PokemonBattleSimulatorRunner.java
+++ src/PokemonBattleSimulatorRunner.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class PokemonBattleSimulatorRunner {
+import java.util.Scanner;
+public class PokemonBattleSimulatorRunner
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Pokemon Battle Simulator!");
+        System.out.print("Are you a new or returning player? ");
+        String returning = scan.nextLine();
+
+
+
+
+
+
+
+
+
+    }
 }
