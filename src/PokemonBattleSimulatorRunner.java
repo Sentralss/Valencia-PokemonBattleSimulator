66155706f79183de4bab7adf8c0c6b9a27d8ed17
Sentrals <bryantv6166@gmail.com>
2023-11-17 17:17:56 -0500

    new commit

--- src/PokemonBattleSimulatorRunner.java
+++ src/PokemonBattleSimulatorRunner.java
@@ -5,31 +5,24 @@ public class PokemonBattleSimulatorRunner
     {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\nWelcome to the Pokemon Battle Simulator!");
-        System.out.println("\n" +
-                "────────▄███████████▄────────\n" +
-                "─────▄███▓▓▓▓▓▓▓▓▓▓▓███▄─────\n" +
-                "────███▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███────\n" +
-                "───██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██───\n" +
-                "──██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██──\n" +
-                "─██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██─\n" +
-                "██▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓██\n" +
-                "██▓▓▓▓▓▓▓▓██░░░░░██▓▓▓▓▓▓▓▓██\n" +
-                "██▓▓▓▓▓▓▓██░░███░░██▓▓▓▓▓▓▓██\n" +
-                "███████████░░███░░██████████\n" +
-                "██░░░░░░░██░░███░░██░░░░░░░██\n" +
-                "██░░░░░░░░██░░░░░██░░░░░░░░██\n" +
-                "██░░░░░░░░░███████░░░░░░░░░██\n" +
-                "─██░░░░░░░░░░░░░░░░░░░░░░░██─\n" +
-                "──██░░░░░░░░░░░░░░░░░░░░░██──\n" +
-                "───██░░░░░░░░░░░░░░░░░░░██───\n" +
-                "────███░░░░░░░░░░░░░░░███────\n" +
-                "─────▀███░░░░░░░░░░░███▀─────\n" +
-                "────────▀███████████▀────────");
-        System.out.print("\nAre you a new or returning player? ");
+
+
+        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓\nWelcome to the Pokemon Battle Simulator!\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓" +
+                "\n             _" +
+                "\n _ __   ___ | | _____ _ __ ___   ___  _ __" +
+                "\n| '_ \\ / _ \\| |/ / _ \\ '_ ` _ \\ / _ \\| '_ \\ " +
+                "\n| |_) | (_) |   <  __/ | | | | | (_) | | | |" +
+                "\n| .__/ \\___/|_|\\_\\___|_| |_| |_|\\___/|_| |_|" +
+                "\n|_|\n" +
+                "\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
+
+        System.out.print("\nAre you a new or returning player? (Type n for new or r for returning) ");
         String returning = scan.nextLine();
-        System.out.print("What game mode do you want to play? (1v1, 2v2, or 3v3): " );
+        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
+        System.out.print("\nWhat game mode do you want to play? (1v1, 2v2, or 3v3): " );
         String gameMode = scan.nextLine();
+        System.out.print("Playing " + gameMode + "...\n" );
+        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
 
 
 
