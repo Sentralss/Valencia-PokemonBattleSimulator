7446b2ecca28099e05a9d11d8fbb5594bc55934e
BT_3W2_25 <bryantv2789@nycstudents.net>
2023-11-20 12:35:25 -0500

    new

--- src/PokemonBattleSimulatorRunner.java
+++ src/PokemonBattleSimulatorRunner.java
@@ -7,20 +7,19 @@ public class PokemonBattleSimulatorRunner
     static String Yellow = "\u001b[33m";
     static String Blue = "\u001b[34m";
     static String Magenta = "\u001b[35m";
-    static String Cyan = "\u001b[36m";
+    static String BlueBright = "\033[0;94m";
     static String White = "\u001b[37m";
-    static String BrightBlack = "\u001b[30;1m";
-    static String BrightRed = "\u001b[31;1m";
-    static String BrightGreen = "\u001b[32;1m";
-    static String BrightYellow = "\u001b[33;1m";
-    static String BrightBlue = "\u001b[34;1m";
-    static String BrightMagenta = "\u001b[35;1m";
-    static String BrightCyan = "\u001b[36;1m";
-    static String BrightWhite = "\u001b[37;1m";
+    static String RedBright = "\033[0;91m";
+    static String PurpleBright = "\033[0;95m";
+    static String CyanBright = "\033[0;96m";
+    static String GreenBright = "\033[0;92m";
     static String Reset = "\u001b[0m";
+
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+
+
 
         System.out.println("""
 
@@ -43,19 +42,14 @@ public class PokemonBattleSimulatorRunner
         String gameMode = scan.nextLine();
         System.out.print("Playing " + gameMode + "...\n" );
         System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
-        System.out.println("""
-                Pick your Pokemon/Team (Can't pick the same Pokemon twice)
-
-                Venasaur   Charizard   Blastoise   Snorlax   Pikachu   Alakazam
-                Grass      Fire        Water       Normal    Electric  Psychic
-
-                Lucario   Golem   Garchomp   Pidgetto   Scizor   Nidoking
-                Fighting  Rock    Ground     Flying     Bug      Poison
-
-                Umbreon   Gengar   Walrein   Metagross   Dragonite   Gardevoir
-                Dark      Ghost    Ice       Steel       Dragon      Fairy
-                
-                Your pick/s (Pick1, Pick2, Pick3):""");
+        System.out.println("Pick your Pokemon/Team (Can't pick the same Pokemon twice)");
+        System.out.println(Green +"Venasaur   "+ Red +"Charizard   "+ Blue +"Blastoise   "+ Reset +"Snorlax   "+ Yellow +"Pikachu");
+        System.out.println(Green +"Grass      "+ Red +"Fire        "+ Blue +"Water       "+ Reset +"Normal    "+ Yellow +"Electric\n");
+        System.out.println(RedBright+"Lucario   "+ GreenBright +"Scizor   "+ Magenta +"Nidoking   "+ Black +"Umbreon   "+ PurpleBright +"Gengar");
+        System.out.println(RedBright+"Fighting  "+ GreenBright +"Bug      "+ Magenta +"Poison     "+ Black +"Dark      "+ PurpleBright +"Ghost\n");
+        System.out.println(CyanBright+"Walrein   "+ White +"Metagross   "+ BlueBright +"Dragonite   ");
+        System.out.println(CyanBright+"Ice       "+ White +"Steel       "+ BlueBright +"Dragon    \n");
+        System.out.println(Reset + "Your pick/s (Pick1, Pick2, Pick3)");
         String pokemon = scan.nextLine();
         System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
     }
