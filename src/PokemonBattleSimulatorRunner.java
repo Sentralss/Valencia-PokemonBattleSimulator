60d187c0b20bb2b2360ccca0544a450598c250c3
Sentrals <bryantv6166@gmail.com>
2023-11-19 22:26:36 -0500

    new commit

--- src/PokemonBattleSimulatorRunner.java
+++ src/PokemonBattleSimulatorRunner.java
@@ -12,20 +12,20 @@ public class PokemonBattleSimulatorRunner
                 ◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓
                              _
                  _ __   ___ | | _____ _ __ ___   ___  _ __
-                | '_ \\ / _ \\| |/ / _ \\ '_ ` _ \\ / _ \\| '_ \\\s
+                | '_ \\ / _ \\| |/ / _ \\ '_ ` _ \\ / _ \\| '_ \\
                 | |_) | (_) |   <  __/ | | | | | (_) | | | |
                 | .__/ \\___/|_|\\_\\___|_| |_| |_|\\___/|_| |_|
                 |_|
 
                 ◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓""");
 
-        System.out.print("\nAre you a new or returning player? (Type n for new or r for returning) ");
+        System.out.print("Are you a new or returning player? (Type n for new or r for returning) ");
         String returning = scan.nextLine();
-        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
-        System.out.print("\nWhat game mode do you want to play? (1v1, 2v2, or 3v3): " );
+        System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
+        System.out.print("What game mode do you want to play? (1v1, 2v2, or 3v3): " );
         String gameMode = scan.nextLine();
         System.out.print("Playing " + gameMode + "...\n" );
-        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
+        System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
         System.out.println("""
                 Pick your Pokemon/Team (Can't pick the same Pokemon twice)
 
@@ -36,7 +36,10 @@ public class PokemonBattleSimulatorRunner
                 Fighting  Rock    Ground     Flying     Bug      Poison
 
                 Umbreon   Gengar   Walrein   Metagross   Dragonite   Gardevoir
-                Dark      Ghost    Ice       Steel       Dragon      Fairy""");
-        System.out.println("\n◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
+                Dark      Ghost    Ice       Steel       Dragon      Fairy
+                
+                Your pick/s (Pick1, Pick2, Pick3):""");
+        String pokemon = scan.nextLine();
+        System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
     }
 }
