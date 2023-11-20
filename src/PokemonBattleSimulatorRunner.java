355dc58cb3d186aa891b09d57fee8911e41f9796
Sentrals <bryantv6166@gmail.com>
2023-11-19 22:54:01 -0500

    new commit

--- src/PokemonBattleSimulatorRunner.java
+++ src/PokemonBattleSimulatorRunner.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 public class PokemonBattleSimulatorRunner
 {
+    static String Black = "\u001b[30m";
+    static String Red = "\u001b[31m";
+    static String Green = "\u001b[32m";
+    static String Yellow = "\u001b[33m";
+    static String Blue = "\u001b[34m";
+    static String Magenta = "\u001b[35m";
+    static String Cyan = "\u001b[36m";
+    static String White = "\u001b[37m";
+    static String BrightBlack = "\u001b[30;1m";
+    static String BrightRed = "\u001b[31;1m";
+    static String BrightGreen = "\u001b[32;1m";
+    static String BrightYellow = "\u001b[33;1m";
+    static String BrightBlue = "\u001b[34;1m";
+    static String BrightMagenta = "\u001b[35;1m";
+    static String BrightCyan = "\u001b[36;1m";
+    static String BrightWhite = "\u001b[37;1m";
+    static String Reset = "\u001b[0m";
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
@@ -22,7 +39,7 @@ public class PokemonBattleSimulatorRunner
         System.out.print("Are you a new or returning player? (Type n for new or r for returning) ");
         String returning = scan.nextLine();
         System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
-        System.out.print("What game mode do you want to play? (1v1, 2v2, or 3v3): " );
+        System.out.print("What game mode do you want to play? (1v1, 2v2, or 3v3): ");
         String gameMode = scan.nextLine();
         System.out.print("Playing " + gameMode + "...\n" );
         System.out.println("◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓◓");
